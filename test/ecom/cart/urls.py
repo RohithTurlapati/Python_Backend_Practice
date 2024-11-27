3275e4514929865f203a79051697c40e4511676c
rohith <rohithturlapati2396@gmail.com>
2024-11-28 00:04:57 +0530

    check

--- test/ecom/cart/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from cart.views import UserListCreateAPIView
-
-
-urlpatterns = [
-    path("", UserListCreateAPIView.as_view()),
-]
--- test/ecom/cart/views/views.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.generics import ListCreateAPIView
-from cart.models import User
-from cart.serializers import UserSerializer
-
-
-class UserListCreateAPIView(ListCreateAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
