16aa840b91ae1b06afd57d26d252b8ebea568a4a
rohith <rohithturlapati2396@gmail.com>
2024-12-26 02:30:16 +0530

    basic type of date printing

--- streamlitpractice/HelloApp.py
+++ streamlitpractice/HelloApp.py
@@ -1,3 +1,22 @@
 import streamlit as st
 
+
+st.header("My first streamlit app")
+st.subheader("Introduction")
+st.text("Streamlit is a web framework for python used in especially in data science, machine learning and AI")
+st.markdown("**Note:** *This is just a practice session*")
+st.markdown("---")
+st.latex(r"\begin{pmatrix}a&b&c\\d&e&f\\g&h&i\end{pmatrix}")
+code ="""
+public static void main(String args[]){
+    system.out.print("Hello world");
+}
+"""
+st.code(code, language="java")
+data = {"a": "1,2,3",
+        "b": "4,5,6",
+        "c": "7,8,9"
+        }
+st.json(data)
+
 st.write("hello world")
